4a5a62429d1b555dce8fbc99c31797b5ef011a02
HeshamM-088 <heshamkhalil1988@gmail.com>
2025-10-21 14:25:01 +0300

    use state init

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
+import { useState } from "react";
 import Footer from "./components/Footer";
 import Home from "./components/Home";
 import NavBar from "./components/NavBar";
 
 const App = () => {
-  let myNumber = 100;
+  let [myNumber, setMyNumber] = useState(100);
+
+  let [status, setstatus] = useState(false);
 
   const increment = () => {
-    myNumber += 1;
-    console.log(myNumber);
+    setMyNumber((prev) => (prev += 1));
   };
 
   return (
@@ -18,9 +20,18 @@ const App = () => {
         increment
       </button>
 
+      <button
+        className="btn btn-success"
+        onClick={() => setstatus((prev) => !prev)}
+      >
+        change Status
+      </button>
+
       <Home myNumber={myNumber} />
       <hr />
       <Footer myNumber={myNumber} />
+
+      {status ? <h1>Lorem ipsum dolor sit amet.</h1> : <h1>empty</h1>}
     </div>
   );
 };
